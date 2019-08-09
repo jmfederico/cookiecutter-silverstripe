26e71ffa4a0a329dd79951e82687753919900f91
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-08-09 12:02:42 +0200

    Fix .gitignore generation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,26 +1,34 @@
+"""Actions to be run after a successful Cookie bake."""
 import glob
-import urllib.request
 from pathlib import Path
-
-urllib.request.urlretrieve(
-    "https://www.gitignore.io/api/node,composer", "../.gitignore"
-)
-
-# Move root files.
-_root = Path("_root")
-project_root = Path("..")
-for child in _root.iterdir():
-    child.rename(project_root / child.name)
-_root.rmdir()
+from urllib.request import Request, urlopen
 
 
-Path("../.gitignore").open("a").write(
-    """
+def run():
+    """Actions to be run."""
+    req = Request(
+        url="https://www.gitignore.io/api/node,composer",
+        headers={"User-Agent": "Python"},
+    )
+    Path("../.gitignore").write_bytes(urlopen(req).read())
+
+    # Move root files.
+    _root = Path("_root")
+    project_root = Path("..")
+    for child in _root.iterdir():
+        child.rename(project_root / child.name)
+    _root.rmdir()
+
+    Path("../.gitignore").open("a").write(
+        """
 ### Silverstripe ###
 public/assets/
 public/resources/
 """
-)
+    )
 
-for path in glob.glob('../**/.cookiecutter-keep', recursive=True):
-    Path(path).unlink()
+    for path in glob.glob("../**/.cookiecutter-keep", recursive=True):
+        Path(path).unlink()
+
+
+run()
